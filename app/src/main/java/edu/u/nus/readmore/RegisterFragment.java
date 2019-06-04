0ee56110fa3d3567f421465861eb85b02a4b4f1a
Jeremy Loh <jeremyloh03@gmail.com>
2019-06-04 21:26:22 +0800

    Added logout menu xml, ability to create email based account in RegisterFragment

--- app/src/main/java/edu/u/nus/readmore/RegisterFragment.java
+++ app/src/main/java/edu/u/nus/readmore/RegisterFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,10 +16,25 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterFragment extends Fragment {
-    TextView alreadyMember;
-    Button createAccBtn;
-    EditText email, password, confirmPassword;
+    private TextView alreadyMember;
+    private Button createAccBtn;
+    private EditText email, password, confirmPassword;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "EmailPassword";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -48,8 +64,7 @@ public class RegisterFragment extends Fragment {
         });
     }
 
-    // creating new user account
-    private void registerNewUser() {
+    private boolean validateForm() {
         boolean validRegistration = true;
         // check for valid email
         String emailRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
@@ -65,8 +80,7 @@ public class RegisterFragment extends Fragment {
             validRegistration = false;
         }
 
-        if (TextUtils.isEmpty(givenPassword) || TextUtils.isEmpty(givenConfirmPassword)
-            || !givenPassword.equals(givenConfirmPassword)) {
+        if (!givenPassword.equals(givenConfirmPassword)) {
             Toast.makeText(getActivity().getApplicationContext(),
                     "Password given does not match",
                     Toast.LENGTH_SHORT)
@@ -82,13 +96,54 @@ public class RegisterFragment extends Fragment {
                     .show();
             validRegistration = false;
         }
+        return validRegistration;
+    }
 
-        if (validRegistration) {
-            Toast.makeText(getActivity().getApplicationContext(),
-                    "Welcome!",
-                    Toast.LENGTH_SHORT)
-                    .show();
+    // creating new user account
+    private void registerNewUser() {
+        if (validateForm()) {
+            // Create a new user account
+            String givenEmail = email.getText().toString();
+            String givenPassword = password.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(givenEmail, givenPassword)
+                    .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                // If the new account was created, the user is also signed in,
+                                // and the AuthStateListener runs the onAuthStateChanged callback.
+                                // In the callback, you can manage the work of sending the
+                                // verification email to the user
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(getActivity().getApplicationContext(),
+                                        "Welcome!",
+                                        Toast.LENGTH_SHORT)
+                                        .show();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(getActivity().getApplicationContext(),
+                                        "Authentication failed.",
+                                        Toast.LENGTH_SHORT)
+                                        .show();
+                                updateUI(null);
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
             backToLogin();
+        } else {
+            email.setText(null);
+            password.setText(null);
+            confirmPassword.setText(null);
         }
     }
 
@@ -121,4 +176,3 @@ public class RegisterFragment extends Fragment {
 //        return false;
 //    }
 }
-
