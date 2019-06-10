3ca6624bcc4dc517c4578f9cd06c49a223e6dbd3
Jeremy Loh <jeremyloh03@gmail.com>
2019-06-10 14:22:33 +0800

    Added Article class

---
+++ app/src/main/java/edu/u/nus/readmore/Article.java
@@ -0,0 +1,37 @@
+package edu.u.nus.readmore;
+
+class Article {
+    private String title;
+    private String description;
+    private String pageid;
+    private String URL;
+    private String imageURL;
+
+    public Article(String title, String description, String pageid, String URL, String imageURL) {
+        this.title = title;
+        this.description = description;
+        this.pageid = pageid;
+        this.URL = URL;
+        this.imageURL = imageURL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPageid() {
+        return pageid;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+}
