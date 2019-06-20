786988554a1a525c6f909ca6b5766d7971c3a28c
Jeremy Loh <jeremyloh03@gmail.com>
2019-06-20 19:13:05 +0800

    Added reset password function

--- app/src/main/java/edu/u/nus/readmore/ResetPasswordFragment.java
+++ app/src/main/java/edu/u/nus/readmore/ResetPasswordFragment.java
@@ -5,36 +5,81 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetPasswordFragment extends Fragment {
     Button resetPwBtn;
+    EditText userEmailEditText;
+
+    private FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         getActivity().setTitle("Reset Password");
-        resetPwBtn = getActivity().findViewById(R.id.resetpw_btn);
+        resetPwBtn = getActivity().findViewById(R.id.reset_pw_btn);
+        userEmailEditText = getActivity().findViewById(R.id.user_email_text);
 
         resetPwBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity().getApplicationContext(),
-                        "Reset link have been sent to your e-mail",
-                        Toast.LENGTH_LONG).show();
-                getActivity()
-                        .getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.intermediate_frame_layout, new LoginFragment())
-                        .commit();
-
+                // Verify email format
+                String email = userEmailEditText.getText().toString();
+                if (validEmailFormat(email)) {
+                    // Send reset password email
+                    mFirebaseAuth.sendPasswordResetEmail(email)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getActivity().getApplicationContext(),
+                                                "Reset link have been sent to your e-mail",
+                                                Toast.LENGTH_LONG)
+                                                .show();
+                                        getActivity()
+                                                .getSupportFragmentManager()
+                                                .beginTransaction()
+                                                .replace(R.id.intermediate_frame_layout, new LoginFragment())
+                                                .commit();
+                                    } else {
+                                        makeToastMessage("Unable to send reset password email",
+                                                Toast.LENGTH_LONG, 0, -80);
+                                    }
+                                }
+                            });
+                }
             }
         });
+    }
+
+    private boolean validEmailFormat(String email) {
+        String emailRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        if (TextUtils.isEmpty(email) || !email.matches(emailRegex)) {
+            makeToastMessage("Please enter a valid Email",
+                    Toast.LENGTH_SHORT, 0, -80);
+            return false;
+        }
+        return true;
+    }
+
+    private void makeToastMessage(String message, int toastLength, int xOffset, int yOffset) {
+        Toast toast = Toast.makeText(getActivity().getApplicationContext(),
+                message,
+                toastLength);
+        toast.setGravity(Gravity.CENTER, xOffset, yOffset);
+        toast.show();
     }
 
     @Nullable
