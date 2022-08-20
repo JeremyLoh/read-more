a7c2f7bdea5bc97f0653f7c0773b174b853892f1
JeremyLoh <jeremyloh03@gmail.com>
2022-08-21 00:44:28 +0800

    Refactor initial image query

--- app/src/main/java/edu/u/nus/readmore/FetchArticleData.java
+++ app/src/main/java/edu/u/nus/readmore/FetchArticleData.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.text.TextUtils;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,7 +24,6 @@ public class FetchArticleData extends AsyncTask<String, Void, Map<String, String
     AsyncArticleResponse articleResponse = null;
     private Map<String, String> output = new HashMap<>();
 
-
     public FetchArticleData(AsyncArticleResponse asyncArticleResponse) {
         this.articleResponse = asyncArticleResponse;
     }
@@ -33,86 +34,45 @@ public class FetchArticleData extends AsyncTask<String, Void, Map<String, String
         MainActivity.getActivityInstance().enableProgressBar(View.VISIBLE);
     }
 
-
     @Override
     protected Map<String, String> doInBackground(String... pageID) {
+        String pageid = pageID[0];
+        output.put("pageid", pageid);
         try {
-            String pageid = pageID[0];
-            output.put("pageid", pageid);
-            // Obtain largest possible image from query
-            // Query with valid thumbnail image (Example)
-            // https://en.wikipedia.org/w/api.php?action=query&format=json&pageids=23420193&prop=pageimages|images&pithumbsize=1000
-            // Query with no valid thumbnail image (Example)
-            // https://en.wikipedia.org/w/api.php?action=query&format=json&pageids=9127632&prop=pageimages|images&pithumbsize=1000
-            String imageQuery = "https://en.wikipedia.org/w/api.php?" +
-                    "action=query&format=json" +
-                    "&pageids=" +
-                    pageid +
-                    "&prop=pageimages|images" +
-                    "&pithumbsize=1000"; // set max image size obtained to be within 1000px
-            String imageData = queryURL(new URL(imageQuery));
-            // parse data obtained
-            JSONObject queryObject = (new JSONObject(imageData)).getJSONObject("query");
-            JSONObject pagesObject = queryObject.getJSONObject("pages");
-            JSONObject pageIDObject = pagesObject.getJSONObject(pageid);
-            // check for existing thumbnail JSON object (from pageimages query)
-            if (pageIDObject.has("thumbnail")) {
-                JSONObject imageInfo = pageIDObject.getJSONObject("thumbnail");
-                String imageSrc = imageInfo.getString("source");
-                output.put("imageURL", imageSrc);
+            String thumbnailImageQuery = getThumbnailImageQuery(pageid);
+            String imageData = queryURL(new URL(thumbnailImageQuery));
+            JSONObject pageResult = parsePageResult(pageid, imageData);
+            if (hasExistingThumbnail(pageResult)) {
+                output.put("imageURL", getThumbnailUrl(pageResult));
             } else {
                 // check for existing images JSONArray
-                if (pageIDObject.has("images")) {
+                if (hasImages(pageResult)) {
                     // get title of first image file in images JSONArray,
                     // to conduct a query for it
-                    JSONArray imageArray = pageIDObject.getJSONArray("images");
+                    JSONArray imageArray = pageResult.getJSONArray("images");
                     int imageArraySize = imageArray.length();
+                    if (imageArraySize == 0) {
+                        // No image available
+                        output.put("imageURL", "");
+                    }
                     if (imageArraySize >= 1) {
                         // Obtain first image that has a valid extension (not svg)
-                        String imageTitle = "";
-                        boolean foundValidImage = false;
-                        for (int index = 0; index < imageArraySize; index++) {
-                            JSONObject imageInfo = imageArray.getJSONObject(index);
-                            imageTitle = imageInfo.getString("title");
-                            // check image extension
-                            String imageExtension = imageTitle.substring(imageTitle.length() - 3);
-                            if (imageExtension.equals("jpg") || imageExtension.equals("png")) {
-                                foundValidImage = true;
-                                break;
-                            }
-                        }
+                        String imageTitle = getFirstValidImageTitle(imageArray);
+                        boolean foundValidImage = !imageTitle.equals("");
                         if (foundValidImage) {
-                            // Query in format: https://en.wikipedia.org/w/api.php?
-                            // action=query&format=json&prop=pageimages&pithumbsize=1000
-                            // &titles=Image:[IMAGE_FILE_TITLE]
-                            // Query image size max of 1000 (&pithumbsize=1000)
-                            String imageTitleQuery = "https://en.wikipedia.org/w/api.php?" +
-                                    "action=query&format=json" +
-                                    "&prop=pageimages" +
-                                    "&pithumbsize=1000" +
-                                    "&titles=Image:" + imageTitle;
+                            String imageTitleQuery = getImageTitleQuery(imageTitle);
                             JSONObject imageJSON = new JSONObject(queryURL(new URL(imageTitleQuery)));
-                            // Retrieving image link through obtained JSON (imageJSON)
-                            JSONObject imageQueryObj = imageJSON.getJSONObject("query");
-                            JSONObject imagePagesObj = imageQueryObj.getJSONObject("pages");
-                            JSONObject pagesInfoObj = imagePagesObj.getJSONObject("-1");
-                            JSONObject thumbnailObj = pagesInfoObj.getJSONObject("thumbnail");
-                            String imageSrc = thumbnailObj.getString("source");
-                            output.put("imageURL", imageSrc);
+                            output.put("imageURL", getImageUrl(imageJSON));
                         } else {
                             // No image available
                             output.put("imageURL", "");
                         }
-                    } else {
-                        // No image available
-                        output.put("imageURL", "");
                     }
                 } else {
                     // No image available
                     output.put("imageURL", "");
                 }
             }
-
             // fetch article query
             String articleQuery = "https://en.wikipedia.org/w/api.php?" +
                     "action=query&format=json" +
@@ -143,6 +103,76 @@ public class FetchArticleData extends AsyncTask<String, Void, Map<String, String
             e.printStackTrace();
         }
         return output;
+    }
+
+    @NonNull
+    private String getImageUrl(JSONObject image) throws JSONException {
+        JSONObject imageQuery = image.getJSONObject("query");
+        JSONObject imagePages = imageQuery.getJSONObject("pages");
+        JSONObject pageInfo = imagePages.getJSONObject("-1");
+        return getThumbnailUrl(pageInfo);
+    }
+
+    private boolean hasImages(JSONObject pageResult) {
+        return pageResult.has("images");
+    }
+
+    private String getFirstValidImageTitle(JSONArray images) throws JSONException {
+        String imageTitle = "";
+        int size = images.length();
+        for (int index = 0; index < size; index++) {
+            JSONObject imageInfo = images.getJSONObject(index);
+            String title = imageInfo.getString("title");
+            String imageExtension = title.substring(title.length() - 3);
+            if (imageExtension.equals("jpg") || imageExtension.equals("png")) {
+                imageTitle = title;
+                break;
+            }
+        }
+        return imageTitle;
+    }
+
+    @NonNull
+    private String getThumbnailUrl(JSONObject page) throws JSONException {
+        JSONObject imageInfo = page.getJSONObject("thumbnail");
+        return imageInfo.getString("source");
+    }
+
+    private boolean hasExistingThumbnail(JSONObject pageIDObject) {
+        return pageIDObject.has("thumbnail");
+    }
+
+    private JSONObject parsePageResult(String pageid, String imageData) throws JSONException {
+        JSONObject queryObject = (new JSONObject(imageData)).getJSONObject("query");
+        JSONObject pagesObject = queryObject.getJSONObject("pages");
+        return pagesObject.getJSONObject(pageid);
+    }
+
+    @NonNull
+    private String getImageTitleQuery(String imageTitle) {
+        // Query in format: https://en.wikipedia.org/w/api.php?
+        // action=query&format=json&prop=pageimages&pithumbsize=1000
+        // &titles=Image:[IMAGE_FILE_TITLE]
+        // Query image size max of 1000 (&pithumbsize=1000)
+        return "https://en.wikipedia.org/w/api.php?" +
+                "action=query&format=json" +
+                "&prop=pageimages" +
+                "&pithumbsize=1000" +
+                "&titles=Image:" + imageTitle;
+    }
+
+    @NonNull
+    private String getThumbnailImageQuery(String pageid) {
+        // Obtain largest possible image from query
+        // Query with valid thumbnail image (Example)
+        // https://en.wikipedia.org/w/api.php?action=query&format=json&pageids=23420193&prop=pageimages|images&pithumbsize=1000
+        // Query with no valid thumbnail image (Example)
+        // https://en.wikipedia.org/w/api.php?action=query&format=json&pageids=9127632&prop=pageimages|images&pithumbsize=1000
+        return "https://en.wikipedia.org/w/api.php?" +
+                "action=query&format=json" +
+                "&pageids=" + pageid +
+                "&prop=pageimages|images" +
+                "&pithumbsize=1000"; // set max image size obtained to be within 1000px
     }
 
     // Returns a String representing a JSONObject
