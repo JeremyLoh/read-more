5e38ebfb183d7e5dd127f0a0b49c784f565123cf
JeremyLoh <jeremyloh03@gmail.com>
2022-08-21 13:54:43 +0800

    Refactor methods for getting article details

--- app/src/main/java/edu/u/nus/readmore/FetchArticleData.java
+++ app/src/main/java/edu/u/nus/readmore/FetchArticleData.java
@@ -24,8 +24,8 @@ import java.util.stream.Stream;
 
 public class FetchArticleData extends AsyncTask<String, Void, Map<String, String>> {
     private final String MAX_IMAGE_SIZE_IN_PX = "1000";
+    private Map<String, String> article = new HashMap<>();
     AsyncArticleResponse articleResponse;
-    private Map<String, String> output = new HashMap<>();
 
     public FetchArticleData(AsyncArticleResponse asyncArticleResponse) {
         this.articleResponse = asyncArticleResponse;
@@ -38,36 +38,20 @@ public class FetchArticleData extends AsyncTask<String, Void, Map<String, String
     }
 
     @Override
+    protected void onPostExecute(Map<String, String> articleDetails) {
+        articleResponse.processFinish(articleDetails);
+        MainActivity.getActivityInstance().disableProgressBar(View.GONE);
+        super.onPostExecute(articleDetails);
+    }
+
+    @Override
     protected Map<String, String> doInBackground(String... pageID) {
         String pageid = pageID[0];
-        output.put("pageid", pageid);
+        article.put("pageid", pageid);
         try {
-            String thumbnailImageQuery = getThumbnailImageQuery(pageid);
-            String imageData = queryURL(new URL(thumbnailImageQuery));
-            JSONObject pageResult = parsePageResult(pageid, imageData);
-            output.put("imageURL", getImageUrlFromArticle(pageResult));
-            // fetch article query
-            String articleQuery = "https://en.wikipedia.org/w/api.php?" +
-                    "action=query&format=json" +
-                    "&pageids=" + pageid +
-                    "&prop=info|extracts" +
-                    "&inprop=url" +
-                    "&explaintext" +
-                    "&exintro=1";
-            String data = queryURL(new URL(articleQuery));
-            // Parse data obtained
-            JSONObject queryObj = (new JSONObject(data)).getJSONObject("query");
-            JSONObject pagesObj = queryObj.getJSONObject("pages");
-            JSONObject pageIDObj = pagesObj.getJSONObject(pageid);
-            String title = pageIDObj.getString("title");
-            String fullURL = pageIDObj.getString("fullurl");
-            String extract = pageIDObj.getString("extract");
-            // Add additional newlines in extract, (after each paragraph)
-            // remove whitespace at start and end
-            extract = TextUtils.join("\n\n", (extract.trim()).split("\n+"));
-            output.put("title", title);
-            output.put("URL", fullURL);
-            output.put("description", extract);
+            JSONObject page = getArticlePage(pageid);
+            JSONObject imagePage = getImagePage(pageid);
+            setArticleDetails(imagePage, page);
         } catch (MalformedURLException e) {
             // if url given is invalid
             e.printStackTrace();
@@ -78,7 +62,46 @@ public class FetchArticleData extends AsyncTask<String, Void, Map<String, String
             // could not query url
             e.printStackTrace();
         }
-        return output;
+        return article;
+    }
+
+    private void setArticleDetails(JSONObject imagePage, JSONObject page) throws JSONException, IOException {
+        article.put("imageURL", getImageUrlFromArticle(imagePage));
+        article.put("title", page.getString("title"));
+        article.put("URL", page.getString("fullurl"));
+        article.put("description", getDescription(page));
+    }
+
+    @NonNull
+    private String getArticleQuery(String pageid) {
+        return "https://en.wikipedia.org/w/api.php?action=query&format=json" +
+                "&pageids=" + pageid +
+                "&prop=info|extracts" +
+                "&inprop=url" +
+                "&explaintext" +
+                "&exintro=1";
+    }
+
+    @NonNull
+    private JSONObject getArticlePage(String pageid) throws IOException, JSONException {
+        String data = queryUrl(new URL(getArticleQuery(pageid)));
+        JSONObject queryObj = (new JSONObject(data)).getJSONObject("query");
+        JSONObject pages = queryObj.getJSONObject("pages");
+        return pages.getJSONObject(pageid);
+    }
+
+    @NonNull
+    private String getDescription(JSONObject page) throws JSONException {
+        // Add additional newlines in extract, (after each paragraph)
+        String extract = page.getString("extract").trim();
+        return TextUtils.join("\n\n", extract.split("\n+"));
+    }
+
+    @NonNull
+    private JSONObject getImagePage(String pageid) throws IOException, JSONException {
+        String thumbnailImageQuery = getThumbnailImageQuery(pageid);
+        String imageData = queryUrl(new URL(thumbnailImageQuery));
+        return parsePageResult(pageid, imageData);
     }
 
     private String getImageUrlFromArticle(JSONObject page) throws JSONException, IOException {
@@ -101,7 +124,7 @@ public class FetchArticleData extends AsyncTask<String, Void, Map<String, String
             return "";
         }
         String imageTitleQuery = getImageTitleQuery(imageTitle);
-        JSONObject imageJSON = new JSONObject(queryURL(new URL(imageTitleQuery)));
+        JSONObject imageJSON = new JSONObject(queryUrl(new URL(imageTitleQuery)));
         return getImageUrlFromImageTitleQuery(imageJSON);
     }
 
@@ -163,7 +186,6 @@ public class FetchArticleData extends AsyncTask<String, Void, Map<String, String
 
     @NonNull
     private String getThumbnailImageQuery(String pageid) {
-        // Obtain largest possible image from query
         // Query with valid thumbnail image (Example)
         // https://en.wikipedia.org/w/api.php?action=query&format=json&pageids=23420193&prop=pageimages|images&pithumbsize=1000
         // Query with no valid thumbnail image (Example)
@@ -175,7 +197,7 @@ public class FetchArticleData extends AsyncTask<String, Void, Map<String, String
     }
 
     // Returns a String representing a JSONObject
-    private String queryURL(URL URL) throws IOException {
+    private String queryUrl(URL URL) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) URL.openConnection();
         try {
             connection = (HttpURLConnection) URL.openConnection();
@@ -193,12 +215,5 @@ public class FetchArticleData extends AsyncTask<String, Void, Map<String, String
              Stream<String> lines = buffer.lines()) {
             return lines.collect(Collectors.joining());
         }
-    }
-
-    @Override
-    protected void onPostExecute(Map<String, String> stringStringMap) {
-        articleResponse.processFinish(stringStringMap);
-        MainActivity.getActivityInstance().disableProgressBar(View.GONE);
-        super.onPostExecute(stringStringMap);
     }
 }
