5f8cddb716be14b4c6fb9220f4df053fa99cb9fc
JeremyLoh <jeremyloh03@gmail.com>
2022-08-21 18:28:05 +0800

    Refactor onboarding screen

--- app/src/main/java/edu/u/nus/readmore/Startup/OnBoardScreenActivity.java
+++ app/src/main/java/edu/u/nus/readmore/Startup/OnBoardScreenActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,91 +39,93 @@ public class OnBoardScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // making activity full screen must declare before setting content view
+        requestForFullScreenActivity();
+
+        if (hasViewedAppOnboardingTutorial()) {
+            startMainActivity();
+        } else {
+            showOnboardingTutorial();
+        }
+    }
+
+    private void requestForFullScreenActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
 
-        // Check user seen tutorial before
-        if (restorePrefData()) {
-            Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(mainActivityIntent);
-            finish();
-        }
+    private boolean hasViewedAppOnboardingTutorial() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
+        return pref.getBoolean("isIntroOpened", false);
+    }
 
+    private void startMainActivity() {
+        Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(mainActivityIntent);
+        finish();
+    }
+
+    private void showOnboardingTutorial() {
+        setupDeviceScreen();
+        instantiateViews();
+        setupScreenItems();
+    }
+
+    private void setupDeviceScreen() {
         setContentView(R.layout.activity_onboardscreen);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
-        // List of Screen items to be displayed
+    }
 
-        final List<ScreenItem> mList = new ArrayList<>();
-        mList.add(new ScreenItem("Users", "Our app allows you to read even if you wish to remain anonymous! However, you can enjoy a better experience while reading from logging in, so what are you waiting for?", R.drawable.guest_or_user));
-        mList.add(new ScreenItem("Navigating through the app", "Fear of complex navigation through app? Fear not, navigating Read More is a piece of cake! Simply click on the burger icon circled in the image to navigate through our app!", R.drawable.tutorial_1));
-        mList.add(new ScreenItem("Navigating through the app", "As an anonymous user you will only see 2 navigation items available. You can either change the theme of your app through the 'Settings' option or simply press 'Login' to have a better reading experience!", R.drawable.tutorial_2));
-        mList.add(new ScreenItem("Further adventures!", "Once you are logged-in, you can edit your profile, indicate topics that are of your interest, browse your read history and even change the theme of the app!", R.drawable.tutorial_3));
-        mList.add(new ScreenItem("What are you waiting for?", "Click on the 'Get Started' button to enjoy reading randomised article at your fingertip! Have fun!", R.drawable.read_more_logo));
-
-        // instantiate views
+    private void instantiateViews() {
         screenPager = findViewById(R.id.screen_viewpager);
         getStartedBtn = findViewById(R.id.btn_get_started);
         nextBtn = findViewById(R.id.btn_next);
         skipTV = findViewById(R.id.tv_skip);
         tabIndicator = findViewById(R.id.tab_indicator);
         btnAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_animation);
+    }
 
-        // setup ViewPager
-        onBoardScreenPagerAdapter = new OnBoardScreenPagerAdapter(this, mList);
-        screenPager.setAdapter(onBoardScreenPagerAdapter);
-
-        // setup TabLayout with ViewPager
-        tabIndicator.setupWithViewPager(screenPager);
-
-        // button click setup
-        nextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                position = screenPager.getCurrentItem();
-                if (position < mList.size()) {
-                    position++;
-                    screenPager.setCurrentItem(position);
-                }
-
-                if (position == mList.size() - 1) {
-                    loadLastScreen();
-                }
-
-            }
-        });
-
-        skipTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(mainActivityIntent);
-                skipTV.setClickable(false);
-                savePrefsData();
-                finish();
-            }
-        });
-
-        getStartedBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(mainActivityIntent);
-                skipTV.setClickable(false);
-                // Prevent user seeing tutorial again after clicking getStarted
-                savePrefsData();
-                finish();
-            }
-        });
-
+    private void setupScreenItems() {
+        final List<ScreenItem> screenItems = getScreenItems();
+        setupViewPager(screenItems);
+        setupTabLayoutWithViewPager();
+        setupButtonClicks(screenItems);
         // TabLayout change according to listener
+        setupTabIndicator(screenItems.size());
+    }
+
+    @NonNull
+    private List<ScreenItem> getScreenItems() {
+        final List<ScreenItem> screenItems = new ArrayList<>();
+        screenItems.add(new ScreenItem("Users", "Our app allows you to read even if you wish to remain anonymous! However, you can enjoy a better experience while reading from logging in, so what are you waiting for?", R.drawable.guest_or_user));
+        screenItems.add(new ScreenItem("Navigating through the app", "Fear of complex navigation through app? Fear not, navigating Read More is a piece of cake! Simply click on the burger icon circled in the image to navigate through our app!", R.drawable.tutorial_1));
+        screenItems.add(new ScreenItem("Navigating through the app", "As an anonymous user you will only see 2 navigation items available. You can either change the theme of your app through the 'Settings' option or simply press 'Login' to have a better reading experience!", R.drawable.tutorial_2));
+        screenItems.add(new ScreenItem("Further adventures!", "Once you are logged-in, you can edit your profile, indicate topics that are of your interest, browse your read history and even change the theme of the app!", R.drawable.tutorial_3));
+        screenItems.add(new ScreenItem("What are you waiting for?", "Click on the 'Get Started' button to enjoy reading randomised article at your fingertip! Have fun!", R.drawable.read_more_logo));
+        return screenItems;
+    }
+
+    private void setupViewPager(List<ScreenItem> screenItems) {
+        onBoardScreenPagerAdapter = new OnBoardScreenPagerAdapter(this, screenItems);
+        screenPager.setAdapter(onBoardScreenPagerAdapter);
+    }
+
+    private void setupTabLayoutWithViewPager() {
+        tabIndicator.setupWithViewPager(screenPager);
+    }
+
+    private void setupButtonClicks(List<ScreenItem> screenItems) {
+        nextBtn.setOnClickListener(getNextButtonClickListener(screenItems));
+        skipTV.setOnClickListener(getSkipIntroClickListener());
+        getStartedBtn.setOnClickListener(getSkipIntroClickListener());
+    }
+
+    private void setupTabIndicator(int screenItemsSize) {
         tabIndicator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                if (tab.getPosition() == mList.size() - 1) {
+                if (tab.getPosition() == screenItemsSize - 1) {
                     loadLastScreen();
                 }
             }
@@ -138,6 +142,36 @@ public class OnBoardScreenActivity extends AppCompatActivity {
         });
     }
 
+    @NonNull
+    private View.OnClickListener getSkipIntroClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivityIntent);
+                skipTV.setClickable(false);
+                saveViewedOnboardingIntro();
+                finish();
+            }
+        };
+    }
+
+    private View.OnClickListener getNextButtonClickListener(List<ScreenItem> screenItems) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                position = screenPager.getCurrentItem();
+                if (position < screenItems.size()) {
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+                if (position == screenItems.size() - 1) {
+                    loadLastScreen();
+                }
+            }
+        };
+    }
+
     private void loadLastScreen() {
         nextBtn.setVisibility(View.INVISIBLE);
         getStartedBtn.setVisibility(View.VISIBLE);
@@ -145,16 +179,10 @@ public class OnBoardScreenActivity extends AppCompatActivity {
         getStartedBtn.setAnimation(btnAnimation);
     }
 
-    private boolean restorePrefData() {
-        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
-        Boolean isIntroActivityOpenedBefore = pref.getBoolean("isIntroOpened", false);
-        return isIntroActivityOpenedBefore;
-    }
-
-    private void savePrefsData() {
+    private void saveViewedOnboardingIntro() {
         SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         editor.putBoolean("isIntroOpened", true);
-        editor.commit();
+        editor.apply();
     }
 }
